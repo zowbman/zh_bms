0f1c5c0414b383cc4c25a42a620320b50855b554
zowbman <zhangweibao@kugou.net>
2016-09-29 18:35:57 +0800

    变更：1、菜单列表

---
+++ src/main/java/com/rms/model/po/TUser.java
@@ -0,0 +1,61 @@
+package com.rms.model.po;
+
+/**
+ * 
+ * Title:TUser
+ * Description:用户po类
+ * @author    zwb
+ * @date      2016年9月29日 下午5:49:26
+ *
+ */
+public class TUser {
+    private Integer id;
+
+    private String useraccount;
+
+    private String userpassword;
+
+    private Integer departmentid;
+
+    private Integer addtime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUseraccount() {
+        return useraccount;
+    }
+
+    public void setUseraccount(String useraccount) {
+        this.useraccount = useraccount == null ? null : useraccount.trim();
+    }
+
+    public String getUserpassword() {
+        return userpassword;
+    }
+
+    public void setUserpassword(String userpassword) {
+        this.userpassword = userpassword == null ? null : userpassword.trim();
+    }
+
+    public Integer getDepartmentid() {
+        return departmentid;
+    }
+
+    public void setDepartmentid(Integer departmentid) {
+        this.departmentid = departmentid;
+    }
+
+    public Integer getAddtime() {
+        return addtime;
+    }
+
+    public void setAddtime(Integer addtime) {
+        this.addtime = addtime;
+    }
+}
