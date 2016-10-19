d1870d67468f05b6910c096f8a388a1293cc3240
zowbman <zhangweibao@kugou.net>
2016-10-19 14:29:42 +0800

    新增：1、角色管理

--- src/main/java/com/rms/mapper/TRoleMapper.java
+++ src/main/java/com/rms/mapper/TRoleMapper.java
@@ -1,5 +1,16 @@
 package com.rms.mapper;
 
+import tk.mybatis.mapper.common.Mapper;
 
-public interface TRoleMapper {
+import com.rms.model.po.TRole;
+
+/**
+ * 
+ * Title:TRoleMapper
+ * Description:角色mapper接口
+ * @author    zwb
+ * @date      2016年10月19日 下午1:01:28
+ *
+ */
+public interface TRoleMapper extends Mapper<TRole> {
 }
---
+++ src/main/java/com/rms/model/po/TRoleCustom.java
@@ -0,0 +1,23 @@
+package com.rms.model.po;
+
+import com.rms.data.type.conversion.date.DateDataTypeConversion;
+import com.rms.data.type.conversion.date.vo.DATEFORMAT;
+
+/**
+ * 
+ * Title:TRoleCustom
+ * Description:角色扩展类
+ * @author    zwb
+ * @date      2016年10月19日 下午2:05:04
+ *
+ */
+public class TRoleCustom extends TRole {
+	 /**
+     * 获取添加时间戳转日期结果
+     * @param date
+     * @return
+     */
+    public String getAddTimeToDate(){
+    	return DateDataTypeConversion.timeMillisToDate(getAddtime(), true, DATEFORMAT.YYYY_MM_DD_HH_MM);
+    }
+}
