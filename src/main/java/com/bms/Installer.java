72f149061ffccc15dfcf3f47c5556adadf9fabf1
zowbman <zhangweibao@kugou.net>
2016-11-04 10:19:58 +0800

    新增：1、安装程序类

---
+++ src/main/java/com/bms/Installer.java
@@ -0,0 +1,15 @@
+package com.bms;
+
+/**
+ * 
+ * Title:Installer
+ * Description:安装程序
+ * @author    zwb
+ * @date      2016年11月3日 下午11:07:03
+ *
+ */
+public class Installer {
+	public static void main(String[] args) {
+		
+	}
+}
